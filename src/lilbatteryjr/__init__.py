ea8bd73a9fbe0b886ca2c37e9916e1e817a4a18c
desperandos101 <35909577+desperandos101@users.noreply.github.com>
2026-09-02 19:08:23 -0400

    get_percentage

--- src/lilbatteryjr/__init__.py
+++ src/lilbatteryjr/__init__.py
@@ -1,2 +1,18 @@
-def main() -> None:
-    print("Hello from lilbatteryjr!")
+target_percentage = -1
+
+# Program process that gets target integer
+def get_percentage() -> int:
+    while True:
+        try:
+            num = int(input("At what battery percentage would you like to be notified?\n"))
+        except ValueError:
+            input("Input could not be read as number. Press enter to continue.")
+            continue
+        if num < 0 or num > 100:
+            input("Input is outside of the 0-100 range. Press enter to continue.")
+            continue
+        return num
+
+# Main
+print("Welcome to lilbatteryjr!")
+print(f"Number chosen: {get_percentage()}")
