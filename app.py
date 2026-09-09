89fcbac7754b7d6ed4d445daf3d9acbce19eaa02
Fra-ppe <183779305+F-L-Perez@users.noreply.github.com>
2026-09-09 19:43:43 -0400

    simple gui window with button

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 #testing pythonguis.com
 #command line  import below
 import sys
 
+class fullMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Lil Battery Jr")
+        button = QPushButton("Check Battery Level!")
+
+        #set central widget of window
+        self.setCentralWidget(button)
 
 #one Qapplication per app instance, below calls command line
 app = QApplication(sys.argv)
 
 # the window widget
-window = QWidget()
+window = fullMainWindow()
 window.show() #why are windows hidden by default tho?
 
 app.exec()
